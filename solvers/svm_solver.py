dd62f16992b6f294db3d6e001a1ba4bf5c165653
shanicky <shanicky4ever@gmail.com>
2022-11-29 18:01:30 -0700

    SVM and rf fair pred

--- solvers/svm_solver.py
+++ solvers/svm_solver.py
@@ -18,3 +18,13 @@ class SvmSolver(BaseSolver):
             attribute_name, attribute_value)
         results = self.modelHandler.simple_forward(mutated_data)
         return results
+
+    def _get_fairness_pred(self, attribute_name, attribute_value):
+        outputs = []
+        for attr in self.dataHandler.get_attr_unique(attribute_name):
+            mutated_data = self.dataHandler.mutator.mutate(
+                attribute_name, attr, assign_fair_value=True)
+            outputs.append(self.modelHandler.simple_forward(mutated_data))
+        results = [1 if self._all_num_equal([ou[i] for ou in outputs]) else 0
+                   for i in range(len(outputs[0]))]
+        return results
